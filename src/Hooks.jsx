c9594c28d20f7c048a00505d8c47dc7fb5ba4c37
panappi <119737780+panappi@users.noreply.github.com>
2023-01-19 14:35:58 +0900

    useState使用時のコールバック関数(prev)について

--- src/Hooks.jsx
+++ src/Hooks.jsx
@@ -21,7 +21,13 @@ const Hooks = () => {
       setCountを使わないと、countの値を変更できない
        */}
       <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>Click me</button>
+      {/* <button onClick={() => setCount(count + 1)}>Click me</button> */}
+      <button onClick={() => setCount((prev) => prev + 1)}>Click me</button>
+      {/*
+      setCountの引数にコールバック関数を指定することで
+      動作するたびに現在のcountの値を取得することができる
+      (count+1を引数にするときと同じ結果が得られる)
+       */}
     </>
   );
 };
